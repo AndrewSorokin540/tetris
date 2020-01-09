f3cc881a344fe65a62ca1ca06b38d8a5b077a2a1
Andrey Sorokin <andrewsorokin540@gmail.com>
2020-01-09 18:01:18 +0700

    Updates

---
+++ precache-manifest.5760093b637dfd5d6947f2e2f3832fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc13a7e8903c7e3feb970a70f2d2c12b",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "6d6f2db6dddbfc513528",
+    "url": "/tetris/static/css/main.fb218a2b.chunk.css"
+  },
+  {
+    "revision": "63e3a10195878b00a6d5",
+    "url": "/tetris/static/js/2.1f8dc665.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/tetris/static/js/2.1f8dc665.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d6f2db6dddbfc513528",
+    "url": "/tetris/static/js/main.024c9f7f.chunk.js"
+  },
+  {
+    "revision": "ecf706e8c3ffc08507fb",
+    "url": "/tetris/static/js/runtime-main.e43f7c09.js"
+  }
+]);
